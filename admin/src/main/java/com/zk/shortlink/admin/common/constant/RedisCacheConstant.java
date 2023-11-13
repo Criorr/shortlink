d876e5cefa8950123b460033aefbf7cfc8387ed7
zk <1476869809@qq.com>
2023-11-13 21:04:45 +0800

    feature: 通过分布式锁完善用户注册，防止使用同一用户短时间内大量恶意注册

---
+++ admin/src/main/java/com/zk/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,8 @@
+package com.zk.shortlink.admin.common.constant;
+
+/**
+ *  短链后管系统 Redis 缓存常量类
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
--- admin/src/main/java/com/zk/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/zk/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.zk.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -7,17 +8,29 @@ import com.zk.shortlink.admin.common.convention.exception.ClientException;
 import com.zk.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.zk.shortlink.admin.dao.entity.UserDO;
 import com.zk.shortlink.admin.dao.mapper.UserMapper;
-import com.zk.shortlink.admin.dto.UserRespDTO;
+import com.zk.shortlink.admin.dto.request.UserRegisterReqDTO;
+import com.zk.shortlink.admin.dto.response.UserRespDTO;
 import com.zk.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import static com.zk.shortlink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
+import static com.zk.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NAME_EXIST;
+import static com.zk.shortlink.admin.common.enums.UserErrorCodeEnum.USER_SAVE_ERROR;
 
 /**
  * 用户接口实现层
  */
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
 
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
     @Override
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> queryWrapper =
@@ -29,5 +42,31 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         UserRespDTO result = new UserRespDTO();
         BeanUtils.copyProperties(userDO, result);
         return result;
+    }
+
+    @Override
+    public Boolean hasUsername(String username) {
+        return !userRegisterCachePenetrationBloomFilter.contains(username);
+    }
+
+    @Override
+    public void register(UserRegisterReqDTO requestParams) {
+        if (!hasUsername(requestParams.getUsername())) {
+            throw new ClientException(USER_NAME_EXIST);
+        }
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParams.getUsername());
+        try {
+            if (lock.tryLock()) {
+                int inserted = baseMapper.insert(BeanUtil.toBean(requestParams, UserDO.class));
+                if (inserted < 1) {
+                    throw new ClientException(USER_SAVE_ERROR);
+                }
+                userRegisterCachePenetrationBloomFilter.add(requestParams.getUsername());
+                return;
+            }
+            throw new ClientException(USER_NAME_EXIST);
+        } finally {
+            lock.unlock();
+        }
     }
 }

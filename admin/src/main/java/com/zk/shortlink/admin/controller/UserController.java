d627f3a0b5cf7e8031d5e598c920cf44a9a0657b
zk <1476869809@qq.com>
2023-11-13 21:07:40 +0800

    feature: 开发短链接后管系统用户注册功能

--- admin/src/main/java/com/zk/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/zk/shortlink/admin/controller/UserController.java
@@ -3,13 +3,12 @@ package com.zk.shortlink.admin.controller;
 import cn.hutool.core.bean.BeanUtil;
 import com.zk.shortlink.admin.common.convention.result.Result;
 import com.zk.shortlink.admin.common.convention.result.Results;
-import com.zk.shortlink.admin.dto.UserActualRespDTO;
-import com.zk.shortlink.admin.dto.UserRespDTO;
+import com.zk.shortlink.admin.dto.request.UserRegisterReqDTO;
+import com.zk.shortlink.admin.dto.response.UserActualRespDTO;
+import com.zk.shortlink.admin.dto.response.UserRespDTO;
 import com.zk.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *  用户管理控制层
@@ -23,7 +22,7 @@ public class UserController {
     /**
      * 根据用户名查询用户信息
      */
-    @GetMapping("/api/short-link/admin/v1/user/{username}")
+    @GetMapping("/api/short-link/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
         return Results.success(userService.getUserByUsername(username));
     }
@@ -31,8 +30,26 @@ public class UserController {
     /**
      * 根据用户名查询无脱敏用户信息
      */
-    @GetMapping("/api/short-link/admin/v1/actual/user/{username}")
+    @GetMapping("/api/short-link/v1/actual/user/{username}")
     public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username) {
         return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
+
+    /**
+     * 查询用户名是否存在
+     */
+    @GetMapping("/api/short-link/v1/user/has-username")
+    public Result<Boolean> hasUsername(@RequestParam("username") String username) {
+        return Results.success(userService.hasUsername(username));
+    }
+
+    /**
+     * 用户注册
+     */
+    @PostMapping("/api/short-link/v1/user")
+    public Result<Void> register(@RequestBody UserRegisterReqDTO requestParam) {
+        userService.register(requestParam);
+        return Results.success();
+    }
+
 }

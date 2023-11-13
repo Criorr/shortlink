cc814a86609aea6dd7e3cb4a35be725fa573e4e2
zk <1476869809@qq.com>
2023-11-13 20:42:44 +0800

    feature: 引入并设置布隆过滤器

---
+++ admin/src/main/java/com/zk/shortlink/admin/config/RBloomFilterConfiguration.java
@@ -0,0 +1,22 @@
+package com.zk.shortlink.admin.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/**
+ * 布隆过滤器配置
+ */
+@Configuration
+public class RBloomFilterConfiguration {
+
+    /**
+     * 防止用户注册查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("userRegisterCachePenetrationBloomFilter");
+        cachePenetrationBloomFilter.tryInit(10000000, 0.001);
+        return cachePenetrationBloomFilter;
+    }
+}

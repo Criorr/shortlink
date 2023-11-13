cd12e57f1c2153c913074359c9c1271966b0b0f4
zk <1476869809@qq.com>
2023-11-13 21:11:40 +0800

    feature: 短连接后管系统redis依赖引入和设置

--- admin/src/main/java/com/zk/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/zk/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -6,7 +6,11 @@ import com.zk.shortlink.admin.common.convention.errorcode.IErrorCode;
  */
 public enum UserErrorCodeEnum implements IErrorCode {
     USER_NULL("B000200", "用户记录不存在"),
-    USER_EXIST("B000200", "用户记录不存在");
+    USER_NAME_EXIST("B000201", "用户名已存在"),
+    USER_EXIST("B000202", "用户记录已存在"),
+    USER_SAVE_ERROR("B000203", "用户保存失败");
+
+
 
     private String code;
     private String message;
--- admin/src/main/java/com/zk/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/zk/shortlink/admin/service/UserService.java
@@ -2,7 +2,8 @@ package com.zk.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.zk.shortlink.admin.dao.entity.UserDO;
-import com.zk.shortlink.admin.dto.UserRespDTO;
+import com.zk.shortlink.admin.dto.request.UserRegisterReqDTO;
+import com.zk.shortlink.admin.dto.response.UserRespDTO;
 
 /**
  * 用户接口层
@@ -10,8 +11,24 @@ import com.zk.shortlink.admin.dto.UserRespDTO;
 public interface UserService extends IService<UserDO> {
     /**
      * 根据用户名查询用户信息
+     *
      * @param username 用户名
      * @return 用户返回实体
      */
     UserRespDTO getUserByUsername(String username);
+
+    /**
+     *  查看用户名是否存在
+     *
+     * @param username 用户名
+     * @return 已存在返回 False 否则返回 True
+     */
+    Boolean hasUsername(String username);
+
+    /**
+     *  用户注册
+     *
+     * @param requestParams 注册请求参数
+     */
+    void register(UserRegisterReqDTO requestParams);
 }

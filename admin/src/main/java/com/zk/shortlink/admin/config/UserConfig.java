39823915c0996a9c32d7dc138bed43fbe2faa1c1
zk <1476869809@qq.com>
2023-11-19 18:03:31 +0800

    feature: 开发短链接后管用户上下文封装

---
+++ admin/src/main/java/com/zk/shortlink/admin/config/UserConfig.java
@@ -0,0 +1,26 @@
+package com.zk.shortlink.admin.config;
+
+import com.zk.shortlink.admin.common.biz.user.UserTransmitFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * 用户配置自动装配
+ */
+@Configuration
+public class UserConfig {
+
+    /**
+     * 用户信息传递过滤器
+     */
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter(StringRedisTemplate stringRedisTemplate) {
+        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserTransmitFilter(stringRedisTemplate));
+        registration.addUrlPatterns("/*");
+        registration.setOrder(0);
+        return registration;
+    }
+}

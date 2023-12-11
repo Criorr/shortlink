6d82856b94c907c408bfcd3c98ee6807583232c3
zk <1476869809@qq.com>
2023-12-11 21:05:33 +0800

    feature: 设置短链接项目布隆过滤器配置

---
+++ project/src/main/java/com/zk/shortlink/project/config/RBloomFilterConfiguration.java
@@ -0,0 +1,23 @@
+package com.zk.shortlink.project.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 布隆过滤器配置
+ */
+@Configuration
+public class RBloomFilterConfiguration {
+
+    /**
+     * 防止短链接创建查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> shortLinkCreateCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("shortLinkCreateCachePenetrationBloomFilter");
+        cachePenetrationBloomFilter.tryInit(10000000, 0.001);
+        return cachePenetrationBloomFilter;
+    }
+}

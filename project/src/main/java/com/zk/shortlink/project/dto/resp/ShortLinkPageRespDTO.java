33bd0a0b8767d9a1faf2c06cda5cfff0e37c5df1
zk <1476869809@qq.com>
2023-12-17 16:18:34 +0800

    feature: 短链接分页接口新增创建时间及时间序列化

--- project/src/main/java/com/zk/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
+++ project/src/main/java/com/zk/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -1,5 +1,6 @@
 package com.zk.shortlink.project.dto.resp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -52,5 +53,17 @@ public class ShortLinkPageRespDTO{
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    /**
+     * 描述
+     */
+    private String describe;
 }

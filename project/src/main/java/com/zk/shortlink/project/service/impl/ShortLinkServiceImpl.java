24829b33ee493a78f85e6352bac72de329fa6d2c
zk <1476869809@qq.com>
2024-01-14 13:56:54 +0800

    feature: 短连接系统分页返回域名添加默认http协议

--- project/src/main/java/com/zk/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/zk/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -105,7 +105,11 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 .eq(ShortLinkDO::getDelFlag, 0)
                 .orderByDesc(ShortLinkDO::getCreateTime);
         ShortLinkPageReqDTO resultPage = baseMapper.selectPage(requestParam, queryWrapper);
-        return resultPage.convert(each -> BeanUtil.toBean(each, ShortLinkPageRespDTO.class));
+        return resultPage.convert(each -> {
+            ShortLinkPageRespDTO result = BeanUtil.toBean(each, ShortLinkPageRespDTO.class);
+            result.setDomain("http" + result.getDomain());
+            return result;
+        });
 
     }
 

6ab512ef0ab12ced1fb613b3920345bbc7dfc393
zk <1476869809@qq.com>
2024-03-10 21:08:02 +0800

    feature: 开发单个短链接访问详情功能

--- project/src/main/java/com/zk/shortlink/project/dao/mapper/LinkLocalStatsMapper.java
+++ project/src/main/java/com/zk/shortlink/project/dao/mapper/LinkLocalStatsMapper.java
@@ -2,8 +2,12 @@ package com.zk.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zk.shortlink.project.dao.entity.LinkLocaleStatsDO;
+import com.zk.shortlink.project.dto.req.ShortLinkStatsReqDTO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * 短链接基础访问地区监控持久层
@@ -17,4 +21,20 @@ public interface LinkLocalStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
             "VALUES( #{linkLocaleStats.fullShortUrl}, #{linkLocaleStats.gid}, #{linkLocaleStats.date}, #{linkLocaleStats.cnt}, #{linkLocaleStats.country}, #{linkLocaleStats.province}, #{linkLocaleStats.city}, #{linkLocaleStats.adcode}, NOW(), NOW(), 0) " +
             "ON DUPLICATE KEY UPDATE cnt = cnt +  #{linkLocaleStats.cnt};")
     void shortLinkLocaleStates(@Param("linkLocaleStats") LinkLocaleStatsDO linkLocaleStatsDO);
+
+    /**
+     * 根据短链接获取指定日期内基础地区监控数据
+     */
+    @Select("SELECT " +
+            "    province, " +
+            "    SUM(cnt) AS cnt " +
+            "FROM " +
+            "    t_link_locale_stats " +
+            "WHERE " +
+            "    full_short_url = #{param.fullShortUrl} " +
+            "    AND gid = #{param.gid} " +
+            "    AND date BETWEEN #{param.startDate} and #{param.endDate} " +
+            "GROUP BY " +
+            "    full_short_url, gid, province;")
+    List<LinkLocaleStatsDO> listLocaleByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
 }
---
+++ project/src/main/java/com/zk/shortlink/project/service/ShortLinkStatsService.java
@@ -0,0 +1,18 @@
+package com.zk.shortlink.project.service;
+
+import com.zk.shortlink.project.dto.req.ShortLinkStatsReqDTO;
+import com.zk.shortlink.project.dto.resp.ShortLinkStatsRespDTO;
+
+/**
+ * 短链接监控接口层
+ */
+public interface ShortLinkStatsService {
+
+    /**
+     * 获取单个短链接监控数据
+     *
+     * @param requestParam 获取短链接监控数据入参
+     * @return 短链接监控数据
+     */
+    ShortLinkStatsRespDTO oneShortLinkStats(ShortLinkStatsReqDTO requestParam);
+}

f8cdc5d4ae3a355a6f14f7bc86523aaa3cac95c7
zk <1476869809@qq.com>
2024-03-16 16:24:20 +0800

    feature: 开发短链接监控之分页访问记录接口

--- admin/src/main/java/com/zk/shortlink/admin/controller/ShortLinkStatsController.java
+++ admin/src/main/java/com/zk/shortlink/admin/controller/ShortLinkStatsController.java
@@ -1,8 +1,11 @@
 package com.zk.shortlink.admin.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.zk.shortlink.admin.common.convention.result.Result;
 import com.zk.shortlink.admin.remote.ShortLinkRemoteService;
+import com.zk.shortlink.admin.remote.dto.req.ShortLinkStatsAccessRecordReqDTO;
 import com.zk.shortlink.admin.remote.dto.req.ShortLinkStatsReqDTO;
+import com.zk.shortlink.admin.remote.dto.resp.ShortLinkStatsAccessRecordRespDTO;
 import com.zk.shortlink.admin.remote.dto.resp.ShortLinkStatsRespDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,5 +30,14 @@ public class ShortLinkStatsController {
     @GetMapping("/api/short-link/admin/v1/stats")
     public Result<ShortLinkStatsRespDTO> shortLinkStats(ShortLinkStatsReqDTO requestParam) {
         return shortLinkRemoteService.oneShortLinkStats(requestParam);
+    }
+
+
+    /**
+     * 访问单个短链接指定时间内访问记录监控数据
+     */
+    @GetMapping("/api/short-link/admin/v1/stats/access-record")
+    public Result<IPage<ShortLinkStatsAccessRecordRespDTO>> shortLinkStatsAccessRecord(ShortLinkStatsAccessRecordReqDTO requestParam) {
+        return shortLinkRemoteService.shortLinkStatsAccessRecord(requestParam);
     }
 }
--- admin/src/main/java/com/zk/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/zk/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -7,10 +7,7 @@ import com.alibaba.fastjson2.TypeReference;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.zk.shortlink.admin.common.convention.result.Result;
 import com.zk.shortlink.admin.remote.dto.req.*;
-import com.zk.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
-import com.zk.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
-import com.zk.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
-import com.zk.shortlink.admin.remote.dto.resp.ShortLinkStatsRespDTO;
+import com.zk.shortlink.admin.remote.dto.resp.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -128,6 +125,22 @@ public interface ShortLinkRemoteService {
      */
     default Result<ShortLinkStatsRespDTO> oneShortLinkStats(ShortLinkStatsReqDTO requestParam) {
         String resultBodyStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/stats", BeanUtil.beanToMap(requestParam));
+        return JSON.parseObject(resultBodyStr, new TypeReference<>() {
+        });
+    }
+
+
+    /**
+     * 访问单个短链接指定时间内监控访问记录数据
+     *
+     * @param requestParam 访问短链接监控访问记录请求参数
+     * @return 短链接监控访问记录信息
+     */
+    default Result<IPage<ShortLinkStatsAccessRecordRespDTO>> shortLinkStatsAccessRecord(ShortLinkStatsAccessRecordReqDTO requestParam) {
+        Map<String, Object> stringObjectMap = BeanUtil.beanToMap(requestParam, false, true);
+        stringObjectMap.remove("orders");
+        stringObjectMap.remove("records");
+        String resultBodyStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/stats/access-record", stringObjectMap);
         return JSON.parseObject(resultBodyStr, new TypeReference<>() {
         });
     }
